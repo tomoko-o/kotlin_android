44b6cbbbf31448f8960507c549c90fddf7b00b79
tomoko.ogawa <tomoko.h.1122@gmail.com>
2017-04-11 19:24:28 +0900

    update HelloApp

--- HelloApp/app/src/main/java/com/usaco_pg/helloapp/MainActivity.kt
+++ HelloApp/app/src/main/java/com/usaco_pg/helloapp/MainActivity.kt
@@ -2,11 +2,20 @@ package com.usaco_pg.helloapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun changeText(view: View) {
+//        text_view.text = "Changed from Kotlin"
+        val textView = findViewById(R.id.text_view) as TextView
+        textView.text = "Changed from Kotlin"
     }
 }
