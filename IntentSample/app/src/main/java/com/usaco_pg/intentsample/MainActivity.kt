019fa3597a02ef3ae3e0909abeafb5a35fd9bf20
tomoko.ogawa <tomoko.h.1122@gmail.com>
2017-04-27 22:34:21 +0900

    create IntentSample

---
+++ IntentSample/app/src/main/java/com/usaco_pg/intentsample/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.usaco_pg.intentsample
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.app.AlertDialog
+import android.util.Log
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    companion object Factory {
+        val EXTRA_TEXT = "com.usaco_pg.intentsample.EXTRA_TEXT"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+//        editText.setText("")
+        editText.setText(resources.getText(R.string.blank_text))
+    }
+
+    fun changeActivity(view: View) {
+        val text = editText.text?.toString()
+        if (text == null || text == "") {
+//            Log.d("changeActivity", "no text")
+            AlertDialog.Builder(this)
+//                    .setTitle("Error")
+                    .setTitle(resources.getText(R.string.alert_title))
+//                    .setMessage("テキストを入力してください")
+                    .setMessage(resources.getText(R.string.hint_text))
+                    .setPositiveButton(resources.getText(R.string.alert_button), null)
+                    .show()
+            return
+        }
+        val intent = Intent(this, SubActivity::class.java)
+        intent.putExtra(EXTRA_TEXT, text)
+        startActivity(intent)
+    }
+}
